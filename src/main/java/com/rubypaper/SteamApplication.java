69836551d300cb4236fc1e3c0ecca46fd09b9b96
kkisim <163362240+kkisim@users.noreply.github.com>
2025-06-20 17:47:53 +0900

    start

---
+++ src/main/java/com/rubypaper/SteamApplication.java
@@ -0,0 +1,13 @@
+package com.rubypaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamApplication.class, args);
+	}
+
+}
