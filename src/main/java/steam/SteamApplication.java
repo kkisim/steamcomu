dca5fe882c19acce8eea75aa6205fbd9072ac59b
Kdonggeon <wowdonggun981113@gmail.com>
2025-06-21 02:09:18 +0900

    로그인이랑 회원가입 만들고 컨트롤러 만드려다 끝남

---
+++ src/main/java/steam/SteamApplication.java
@@ -0,0 +1,11 @@
+package steam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SteamApplication.class, args);
+    }
+}
